db1a0c7729433f0bb7eda01e0d7d39430c322fc6
Tonyrealzy <umehobiarinze2@gmail.com>
2024-01-21 14:54:05 +0100

    Just did it

---
+++ connect-4-game/src/Components/Gameboard.js
@@ -0,0 +1,54 @@
+import React, {useState} from "react";
+import '../Components/Game.css';
+import GameCircle from "./GameCircle";
+
+const NO_PLAYER = 0;
+const PLAYER_1 = 1;
+const PLAYER_2 = 2;
+
+const GameBoard = () => {
+    const [gameBoard, setGameBoard] = useState(Array(16).fill(NO_PLAYER));
+    const [currentPlayer, setCurrentPlayer] = useState(PLAYER_1);
+    console.log(gameBoard);
+
+    const circleClicked = (id) => {
+        //console.log("Circle clicked: " + id);
+
+        const board = [...gameBoard];
+        board[id] = currentPlayer;
+        setGameBoard(board);
+
+        // gameBoard[id] = currentPlayer;
+        // setGameBoard(gameBoard);
+
+        setCurrentPlayer(currentPlayer === PLAYER_1 ? PLAYER_2 : PLAYER_1);
+    }
+
+
+    const renderCircle= (id) => {
+        return <GameCircle id={id} className={`player${gameBoard[id]}`} onCircleClicked={circleClicked}/>
+    }
+
+    return (
+        <div className="gameBoard">
+            {renderCircle(0)}
+            {renderCircle(1)}
+            {renderCircle(2)}
+            {renderCircle(3)}
+            {renderCircle(4)}
+            {renderCircle(5)}
+            {renderCircle(6)}
+            {renderCircle(7)}
+            {renderCircle(8)}
+            {renderCircle(9)}
+            {renderCircle(10)}
+            {renderCircle(11)}
+            {renderCircle(12)}
+            {renderCircle(13)}
+            {renderCircle(14)}
+            {renderCircle(15)}
+        </div>
+    )
+}
+
+export default GameBoard;
